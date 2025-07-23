357d2ba869cd416faa191620e8ecfd42840325a2
muhamadjawad <jawadmansoor1998@gmail.com>
2025-07-23 23:49:38 +0500

    colors added

---
+++ src/theme/colors.ts
@@ -0,0 +1,9 @@
+export const colors = {
+  primary: '#2ECC71',
+  secondary: '#F1C40F',
+  background: '#F5F5F5',
+  text: '#333333',
+  white: '#FFFFFF',
+  black: '#000000',
+  grey: '#BDC3C7',
+}; 
