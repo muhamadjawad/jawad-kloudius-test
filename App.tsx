a6a066da970989d198e192b805e8e2a38e1d1afc
muhamadjawad <jawadmansoor1998@gmail.com>
2025-07-24 20:30:09 +0500

    splash screen impemented

--- App.tsx
+++ App.tsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import MapScreen from '@src/screens/MapScreen';
 import { SearchHistoryProvider } from '@src/context/SearchHistoryContext';
+import SplashScreen from 'react-native-splash-screen';
 
 function App(): React.JSX.Element {
+
+  useEffect(() => {
+    if (__DEV__) {
+    } else {
+      setTimeout(() => {
+        SplashScreen.hide();
+      }, 2000);
+    }
+  }, []);
+
   return (
     <SearchHistoryProvider>
       <MapScreen />
