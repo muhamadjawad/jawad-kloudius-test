635c9a6c12266c9c2279733b2ea620d792bd1da8
muhamadjawad <jawadmansoor1998@gmail.com>
2025-07-25 14:57:30 +0500

    use maps hook created

---
+++ src/hooks/useMap.ts
@@ -0,0 +1,70 @@
+import { useRef, useState, useEffect } from 'react';
+import MapView from 'react-native-maps';
+import SplashScreen from 'react-native-splash-screen';
+import { PlaceDetailsType, CoordinatesType } from '@src/types';
+import { useMarkerIndicator } from './useMarkerIndicator';
+
+export const useMap = () => {
+  const mapViewRef = useRef<MapView>(null);
+  const [showPredictions, setShowPredictions] = useState<boolean>(false);
+  const [selectedPlace, setSelectedPlace] = useState<PlaceDetailsType | null>(null);
+  const [isSheetMinimized, setIsSheetMinimized] = useState<boolean>(false);
+  const [markerLocation, setMarkerLocation] = useState<CoordinatesType>({
+    latitude: 24.8756,
+    longitude: 67.0396,
+  });
+
+
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
+  const recenterMap = () => {
+    if (mapViewRef.current) {
+      mapViewRef.current.animateToRegion(
+        {
+          ...markerLocation,
+          latitudeDelta: 0.05,
+          longitudeDelta: 0.05,
+        },
+        1000
+      );
+    }
+  };
+
+  const handleLocationSelect = (details: PlaceDetailsType | null) => {
+    if (details) {
+      const { lat, lng } = details.geometry.location;
+      const newLocation = {
+        latitude: lat,
+        longitude: lng,
+      };
+      setMarkerLocation(newLocation);
+      setSelectedPlace(details);
+      setIsSheetMinimized(false);
+      setShowPredictions(false);
+      mapViewRef.current?.animateToRegion(
+        {
+          ...newLocation,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
+        },
+        1000
+      );
+    } else {
+      setSelectedPlace(null);
+    }
+  };
+
+  return {
+    mapViewRef,
+    showPredictions,
+    setShowPredictions,
+    selectedPlace,
+    isSheetMinimized,
+    setIsSheetMinimized,
+    markerLocation,
+    recenterMap,
+    handleLocationSelect
+  };
+}; 
--- src/screens/MapScreen/index.tsx
+++ src/screens/MapScreen/index.tsx
@@ -1,61 +1,30 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React from 'react';
 import { StyleSheet, View, TouchableOpacity, Text, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
-import SplashScreen from 'react-native-splash-screen';
 import { colors } from '@src/theme/colors';
 import MyLocationIcon from '@src/assets/svgs/MyLocationIcon';
 import SearchBar from '@src/components/SearchBar';
 import BottomSheet, { BOTTOM_SHEET_MIN_HEIGHT } from '@src/components/BottomSheet';
-import { PlaceDetailsType, CoordinatesType } from '@src/types';
 import PlaceInfo from '@src/components/PlaceInfo';
 import MarkerIndicator from '@src/components/MarkerIndicator';
+import { useMap } from '@src/hooks/useMap';
 import { useMarkerIndicator } from '@src/hooks/useMarkerIndicator';
 
 const MapScreen = () => {
-  const mapViewRef = useRef<MapView>(null);
-  const [showPredictions, setShowPredictions] = useState<boolean>(false);
-  const [selectedPlace, setSelectedPlace] = useState<PlaceDetailsType | null>(null);
-  const [isSheetMinimized, setIsSheetMinimized] = useState<boolean>(false);
-  const [markerLocation, setMarkerLocation] = useState<CoordinatesType>({
-    latitude: 24.8756,
-    longitude: 67.0396,
-  });
+  const {
+    mapViewRef,
+    showPredictions,
+    setShowPredictions,
+    selectedPlace,
+    isSheetMinimized,
+    setIsSheetMinimized,
+    markerLocation,
+    recenterMap,
+    handleLocationSelect,
+  } = useMap();
+
   const { rotation, isMarkerVisible, handleRegionChange } = useMarkerIndicator({ markerLocation });
 
-  useEffect(() => {
-    SplashScreen.hide();
-  }, []);
-
-  const recenterMap = () => {
-    if (mapViewRef.current) {
-      mapViewRef.current.animateToRegion({
-        ...markerLocation,
-        latitudeDelta: 0.05,
-        longitudeDelta: 0.05,
-      }, 1000);
-    }
-  };
-
-  const handleLocationSelect = (details: PlaceDetailsType | null) => {
-    if (details) {
-      const { lat, lng } = details.geometry.location;
-      const newLocation = {
-        latitude: lat,
-        longitude: lng,
-      };
-      setMarkerLocation(newLocation);
-      setSelectedPlace(details);
-      setIsSheetMinimized(false);
-      setShowPredictions(false);
-      mapViewRef.current?.animateToRegion({
-        ...newLocation,
-        latitudeDelta: 0.01,
-        longitudeDelta: 0.01,
-      }, 1000);
-    } else {
-      setSelectedPlace(null);
-    }
-  };
 
   return (
     <TouchableWithoutFeedback onPress={() => {
